51a0530d35bf4f17e5f797525dc9634b133b7e0a
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-19 20:41:18 -0500

    :recycle: Update

--- array_to_list.ts
+++ array_to_list.ts
@@ -3,7 +3,24 @@ export interface List<T> {
   rest: List<T> | null;
 }
 
-// Recursivity FTW 🚀🚀🚀
 export function arrayToList<T>([value, ...values]: T[]): List<T> | null {
   return value ? { value, rest: arrayToList(values) } : null;
+}
+
+export function arrayToListReduce<T>(values: T[]) {
+  // it would enter on one line if this were plain JS 🥲
+  return values.reduceRight<List<T> | null>(
+    (rest, value) => ({ value, rest }),
+    null,
+  );
+}
+
+export function arrayToListFor<T>(values: T[]) {
+  let rest = {};
+
+  for (let i = values.length - 1; i >= 0; i--) {
+    rest = { value: values[i], rest };
+  }
+
+  return rest as List<T>;
 }
