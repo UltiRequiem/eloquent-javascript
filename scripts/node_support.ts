f55a3bcd97b913a5ecfc96329b29a832f98b888e
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-19 20:52:20 -0500

    :recycle: Refactoring

---
+++ scripts/node_support.ts
@@ -0,0 +1,10 @@
+import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.12/node_support.ts";
+
+buildPackage({
+  repoName: "eloquent-javascript",
+  package: "eloq",
+  description: "Eloquent JavaScript exercises.",
+  homepage: "https://ulti.js.org/eloquent-javascript",
+  keywords: ["book", "course"],
+  version: "1.0.0",
+});
