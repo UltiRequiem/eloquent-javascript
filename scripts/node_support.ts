af0e6d3d5bfdfa744865e3708f6b9e87d65ce349
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-20 21:29:58 -0500

    :recycle: Update

--- scripts/node_support.ts
+++ scripts/node_support.ts
@@ -2,7 +2,7 @@ import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.12/node_suppor
 
 buildPackage({
   repoName: "eloquent-javascript",
-  package: "eloq",
+  name: "eloq",
   description: "Eloquent JavaScript exercises.",
   homepage: "https://ulti.js.org/eloquent-javascript",
   keywords: ["book", "course"],
---
+++ src/language/deep_equal.ts
@@ -0,0 +1,3 @@
+export function deepEqual(first: unknown, second: unknown) {
+  const [firstType, secondType] = [typeof first, typeof second];
+}
