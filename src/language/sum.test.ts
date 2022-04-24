83925fa5a135575814f98ef83564277969fc8cf7
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-24 15:47:06 -0500

    :white_check_mark: Add tests

--- src/language/sum.test.ts
+++ src/language/sum.test.ts
@@ -1,8 +1,14 @@
-import { sum } from "./sum.ts";
+import { sum, sumFor } from "./sum.ts";
 import { assertEquals } from "../../deps.ts";
 
 Deno.test("[sum]", () => {
   const result = sum(1, 10, 23, 46);
+
+  assertEquals(result, 80);
+});
+
+Deno.test("[sumFor]", () => {
+  const result = sumFor(1, 10, 23, 46);
 
   assertEquals(result, 80);
 });
