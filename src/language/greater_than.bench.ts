6b7afbe6acdf874b9d97094b6fd15bb53179a770
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-01 11:21:07 -0500

    :recycle: Upda

--- src/language/greater_than.bench.ts
+++ src/language/greater_than.bench.ts
@@ -1,1 +1,9 @@
 import { greaterThan } from "./greater_than.ts";
+
+Deno.bench("greaterThan", () => {
+  const greaterOne = greaterThan(1);
+
+  for (let i = 0; i < 100; i++) {
+    greaterOne(i);
+  }
+});
